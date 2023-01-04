68dd2adf78ca9fd0b6f98452e8f778a02d695a36
giuliohome <giuliohome@gmail.com>
2023-01-04 15:59:11 +0100

    Async all the way

--- main.js
+++ main.js
@@ -3,24 +3,13 @@ const fsp = require('fs/promises');
 const jsonfile = require('jsonfile');
 const path = './config.json';
 
-function readHtml(file)
-{
-  try {
-    const data = fs.readFileSync(file, 'utf8');
-    return data;
-  } catch (err) {
-    console.error(err);
-    return null
-  }
-}
-
 async function processJson(input, output)
 {
 
         try {
           data = await jsonfile.readFile(input);
           console.log(`producing output html from ${data.tests.length} tests json`)
-          var htmlTemplate = readHtml('./template.html');
+          var htmlTemplate = await fsp.readFile('./template.html', 'utf8');
           data_str = JSON.stringify(data); // TO-DO producing html
           html_out = htmlTemplate.replace('**JSON-TEST-RESULTS**', data_str)
           await fsp.writeFile(output, html_out);
