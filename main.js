cfcb377d961345970966c4a36d01ff45d06f0757
giuliohome <giuliohome@gmail.com>
2023-01-04 14:48:54 +0100

    .js cleanse

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
 const fs = require('fs');
 
-function buildHtml(req) {
-  var header = '';
-  var body = '';
-
-  // concatenate header string
-  // concatenate body string
-
-  return '<!DOCTYPE html>'
-       + '<html><head>' + header + '</head><body>' + body + '</body></html>';
-};
-
-// var html = buildHtml();
-
 
 function readHtml(file)
 {
