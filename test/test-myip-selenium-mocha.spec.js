e7582f936267bbcf7362d8329fc685c6fa63d930
giuliohome <giuliohome@gmail.com>
2023-01-05 09:40:12 +0100

    mocha with selenium and chai bdd

---
+++ test/test-myip-selenium-mocha.spec.js
@@ -0,0 +1,25 @@
+const { Builder, By, Key, until } = require('selenium-webdriver')
+const assert = require('assert')
+var expect = require('chai').expect;
+
+describe('Selenium Test - User Story', function() {
+  this.timeout(30000)
+  let driver
+  let vars
+  beforeEach(async function() {
+    driver = await new Builder().forBrowser('chrome').build()
+    vars = {}
+  })
+  afterEach(async function() {
+    await driver.quit();
+  })
+  it('IP Read Test - User Story', async function() {
+    await driver.get("http://checkip.dyndns.org/");
+    await driver.manage().window().setRect({ width: 1304, height: 739 });
+
+    const body = await driver.findElement(By.tagName("body"));
+    const body_str = await body.getText()
+    expect(body_str).to.be.a('string').and.satisfy(msg => msg.startsWith('Current IP Address: '));
+
+  })
+})
